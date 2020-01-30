3b6a587441bfd5205358eba26795048e0c2316d2
Petr Stoklas <petrstoklas@gmail.com>
2020-01-30 21:34:43 +0100

    Scroll section

---
+++ src/components/Main/Main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import GraphicDesignImage from '../../images/graphicDesign.png'
+import Section from './Section'
+
+const Main = props => {
+    const text = 'máme rádi tvary. tvoříme vlastní designy z obrázků a fotek, které pak umisťujeme do světa okolo nás. zatím je můžete pověsit na stěnu nebo je najít na oblečení a na dekorativních předmětech.'
+
+    return (
+        <Section
+            imgSrc={GraphicDesignImage}
+            text={text}
+        />
+    )
+}
+
+Main.propTypes = {
+
+}
+
+export default Main
