9c10f2b8a3ddf459b92c3f9e73feb4538e2627a3
Petr Stoklas <petrstoklas@gmail.com>
2020-01-30 21:32:09 +0100

    Main goes to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
+import Layout from "../components/Layout/Layout"
 import SEO from "../components/seo"
+
+import Main from '../components/Main/Main'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Main />
   </Layout>
 )
 
