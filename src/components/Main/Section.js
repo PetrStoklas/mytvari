d44c4705b635cd7db6713de6b740fc67f61774ee
Petr Stoklas <petrstoklas@gmail.com>
2020-01-30 21:40:04 +0100

    Cleaning

--- src/components/Main/Section.js
+++ src/components/Main/Section.js
@@ -3,13 +3,26 @@ import PropTypes from 'prop-types'
 
 const Section = ({ text, imgSrc }) => {
     return (
-        <div style={{ padding: '5%', paddingRight: '0', borderRadius: '0 15px 0 0', backgroundColor: 'white', height: 'calc(100% + 2px)',  }}>
+        <div style={{
+            padding: '5%',
+            paddingRight: '0',
+            borderRadius: '0 15px 0 0',
+            backgroundColor: 'white',
+            height: 'calc(100% + 2px)', 
+        }}>
             <h2>Grafika</h2>
-            <div style={{ width: '60%', margin: 'auto' }}>
+            <div style={{
+                width: '60%',
+                margin: 'auto'
+            }}>
                 <img src={imgSrc} alt="graphic design" style={{ width: '100%' }} />
             </div>
             <br />
-            <p style={{ textAlign: 'justify', textJustify: 'inter-word', margin: 'auto' }}>{text}</p>
+            <p style={{
+                textAlign: 'justify',
+                textJustify: 'inter-word',
+                margin: 'auto'
+            }}>{text}</p>
         </div>
     )
 }
