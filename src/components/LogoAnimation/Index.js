801b45d06d26ce9c2fb5107b3df67abb73163b82
Petr Stoklas <petrstoklas@gmail.com>
2020-02-01 23:10:23 +0100

    Intro animation

--- src/components/LogoAnimation/Index.js
+++ src/components/LogoAnimation/Index.js
@@ -1,30 +1,57 @@
 import React from 'react'
 import styled from 'styled-components'
-import PropTypes from 'prop-types'
-import PicturePart from './PicturePart'
+import Logo from '../../images/logo.png'
 
 const DimmedBackground = styled.div`
     position: absolute;
     top: 0;
     right: 0;
     height: 100vh;
+    line-height: 100vh;
     width: 100vw;
     background-color: rgba(255, 255, 255, .85);
+    animation: fadeout 1s linear 2s;
+
+
+    @keyframes fadeout {
+        from { opacity: .85 }
+        to { opacity: 0 }
+    }
 `
 
-const Index = props => {
+const PictureCover = styled.div`
+    width: 12vw;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    right: 0;
+    background-color: rgba(255, 255, 255, .85);
+    animation: slideWidth 2s ease-in-out;
+
+    @keyframes slideWidth {
+        from { width: 87vw; }
+        to { width: 12vw; }
+    };
+`
+
+const Picture = styled.img`
+    width: 100%;
+    margin: auto;
+    vertical-align: middle;
+`
+
+const Index = () => {
+    
+    setTimeout(() => {
+        if (typeof window !== `undefined`) window.location.replace(`/home`)
+    }, 2700)
+
     return (
-        <>
-            {'logo animation will go here'}
-            <DimmedBackground>
-                <PicturePart />
-            </DimmedBackground>
-        </>
+        <DimmedBackground>
+            <PictureCover />
+            <Picture src={Logo} />
+        </DimmedBackground>
     )
-}
-
-Index.propTypes = {
-
 }
 
 export default Index
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
+import LogoAnimation from '../components/LogoAnimation'
 
-import Layout from "../components/Layout/Layout"
-import SEO from "../components/seo"
-
-import Main from '../components/Main/Main'
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Main />
-  </Layout>
-)
+const IndexPage = () => <LogoAnimation />
 
 export default IndexPage
