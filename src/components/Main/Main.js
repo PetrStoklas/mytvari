5392ab9ba0373ec62ec80e118eef902d2cce02dc
Petr Stoklas <petrstoklas@gmail.com>
2020-02-02 09:56:09 +0100

    Cleaning

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import GraphicDesignImage from '../../images/graphicDesign.png'
 import Section from './Section'
 
-const Main = props => {
+const Main = () => {
     const text = 'máme rádi tvary. tvoříme vlastní designy z obrázků a fotek, které pak umisťujeme do světa okolo nás. zatím je můžete pověsit na stěnu nebo je najít na oblečení a na dekorativních předmětech...'
 
     return (
@@ -12,10 +11,6 @@ const Main = props => {
             text={text}
         />
     )
-}
-
-Main.propTypes = {
-
 }
 
 export default Main
