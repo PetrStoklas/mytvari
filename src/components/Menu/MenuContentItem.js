fbd6050850570b6d2559615e2b1b38f3a1a9865c
Petr Stoklas <petrstoklas@gmail.com>
2020-02-02 12:44:19 +0100

    Menu content item not finishde

---
+++ src/components/Menu/MenuContentItem.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const title = 
+
+const MenuContentItem = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+MenuContentItem.propTypes = {
+
+}
+
+export default MenuContentItem
